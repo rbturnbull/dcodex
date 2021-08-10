eaed9b539fe1c3f5c3b468e27770238a23dcb72d
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2021-08-11 08:45:32 +1000

    moving to poetry and adding github actions

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-requirements = [
-    'Django>3',
-    'django-guardian',
-    'django-polymorphic',
-    'numpy',
-    'pandas>=1.0.5',
-    'scipy',
-    'matplotlib',
-    'python-Levenshtein',
-    'pyxDamerauLevenshtein', # now an optional install
-    'beautifulsoup4',
-    'django-admin-sortable2',
-    'lxml',
-    "regex",
-    "django-filer",
-    'django-imagedeck @ git+https://gitlab.unimelb.edu.au/rturnbull/django-imagedeck.git#egg=django-imagedeck',
-    'gotoh @ git+https://github.com/rbturnbull/gotoh.git#egg=gotoh',
-]
+import setuptools
 
-setup(
-    install_requires=requirements,
-)
-
-
+if __name__ == "__main__":
+    setuptools.setup()
